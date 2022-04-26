b892d668ca318ef0b97da507da934ea5d2d2610d
pryvx <641813525@qq.com>
2022-04-26 14:03:06 +0800

    修改首页README文件

--- nibolan/src/main/java/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-	public static void main(String[] args) {
-		String s = "9+(3-1)*3+10/2";
-		RPNCalculator aa = new RPNCalculator();
-		System.out.println(aa.toRPN(s));
-		System.out.println(aa.calculate(s));
-	}
-}
